c21d7bb94e5fd9da5ce9fb50476f37b184849182
mchlltt <me@northwestern.edu>
2017-03-11 14:06:18 -0600

    Add friendly context object name

--- snippets/views.py
+++ snippets/views.py
@@ -8,16 +8,18 @@ from .models import Snippet
 
 class SnippetList(ListView):
     model = Snippet
+    context_object_name = 'snippets'
 
 
 def home(request):
     if request.method == 'GET':
         form = SnippetForm()
+
     else:
-        # A POST request: Handle Form Upload
         # Bind data from request.POST into a PostForm
         form = SnippetForm(request.POST)
-        # If data is valid, proceeds to create a new post and redirect the user
+
+        # If data is valid, proceeds to create a new snippet and redirect the user to snippets.
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('snippets/')
