fa2c0bc364e5dba868dbf19627e50a67c992c161
mchlltt <me@northwestern.edu>
2017-03-11 12:09:05 -0600

    Add __str__ to Snippet

--- snippets/models.py
+++ snippets/models.py
@@ -6,3 +6,7 @@ class Snippet(models.Model):
     language = models.CharField(max_length=256, blank=False, null=False)
     text = models.TextField(blank=False, null=False)
     description = models.TextField(max_length=256, blank=False, null=False)
+
+    def __str__(self):
+        """ Returns a human readable version of the snippet object """
+        return '\nTitle: %s\n Language: %s\n Text: %s\n Description: %s\n' % (self.title, self.language, self.text, self.description)
